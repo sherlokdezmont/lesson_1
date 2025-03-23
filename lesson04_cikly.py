ac7cbc163877c4e0844302e774d5b0e7d5823d94
unknown <v.dezmo99@gmail.com>
2025-03-23 22:56:31 +0300

    My four lesson 23.03.25

---
+++ lesson04_cikly.py
@@ -0,0 +1,4 @@
+beers = ["ale", "lager", "stout", "porter", "lambic"]
+for beer in beers:
+    print(f'{beer.title()} ahuennoe pivo!!!!!!!!!!!\n\n'.title())
+    print(f'я хочу ещё выпить этого ахуенного пива!!!'.title(), f'{beer.lower()} реально пиздатое пиво')
