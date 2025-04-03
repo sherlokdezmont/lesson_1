900064a1e365ab084d87933000c7b76cb6dfb32a
unknown <v.dezmo99@gmail.com>
2025-04-03 21:57:53 +0300

    03.04.25,dz 4 uroka

---
+++ lesson04_DZ.py
@@ -0,0 +1,4 @@
+library = ["book", "papper", "zhopa", "magazine", "crosswords"]
+for i in library:
+    print(i.title(), f'\nochen polezno chitat, suka blyat!!!'.upper())
+    print(f'Хочу чтобы ты села на меня сейчас!!!!'.title(), f'{i.upper()} и я бы постучал сзади')
